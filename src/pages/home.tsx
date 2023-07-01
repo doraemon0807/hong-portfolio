d75db5790dd1be7dadd497a7275fbb2cc652b2ac
doraemon0807 <98371903+doraemon0807@users.noreply.github.com>
2023-06-30 22:59:24 -0400

    TagCloud

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,53 @@
+import TagCloud from "@frank-mayer/react-tag-cloud";
+import { TagCloudOptions } from "TagCloud";
+
 export default function Home() {
   return (
-    <div className="w-full h-screen grid grid-cols-2 items-center gap-32 px-20">
-      <div className="place-self-center">
+    <div className="w-full h-screen grid grid-cols-2 items-center gap-x-0.5 px-20">
+      <div className="place-self-center tracking-widest">
         <h2 className="text-5xl">Hello, I'm</h2>
         <h1 className="text-7xl font-bold mt-5">Yunseok Hong</h1>
         <div className="text-2xl font-medium mt-10 flex">
           <span>A </span>
-          <ul className="relative">
-            <li className="animate-swapWordAnimation absolute left-0">
+          <ul className="relative font-semibold">
+            <li className="animate-swapWordAnimation tracking-[2px] lg-1 absolute left-0">
               Fullstack
             </li>
-            <li className="animate-swapWordAnimation absolute left-0 animation-delay-3000">
+            <li className="animate-swapWordAnimation absolute left-0 lg-1 animation-delay-3000">
               Frontend
             </li>
-            <li className="animate-swapWordAnimation absolute left-0 animation-delay-6000">
+            <li className="animate-swapWordAnimation tracking-[3px] lg-1 absolute left-0 animation-delay-6000">
               Backend
             </li>
           </ul>
-          <span className="translate-x-24"> Developer Wannabe</span>
+          <span className="translate-x-[114px]"> Developer Wannabe</span>
         </div>
       </div>
       <div className="place-self-center">
-        <div className="w-64 h-64 bg-red-900">Cloud comes here</div>
+        <TagCloud
+          className="text-2xl select-none"
+          options={(w: Window & typeof globalThis): TagCloudOptions => ({
+            radius: Math.min(350, w.innerWidth, w.innerHeight) / 2,
+            maxSpeed: "normal",
+            keep: true,
+          })}
+        >
+          {[
+            "HTML",
+            "CSS",
+            "NodeJS",
+            "Express",
+            "SCSS",
+            "Prisma",
+            "Typescript",
+            "RestAPI",
+            "MongoDB",
+            "React",
+            "NextJS",
+            "TailwindCSS",
+            "Javascript",
+          ]}
+        </TagCloud>
       </div>
     </div>
   );
